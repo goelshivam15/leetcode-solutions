6ed56d08b4804c9381c94ed87af64b672607f4f2
goelshivam15 <93276658+goelshivam15@users.noreply.github.com>
2022-12-25 11:30:56 +0530

    Added solution - LeetHub

--- Union of two arrays - GFG/union-of-two-arrays.cpp
+++ Union of two arrays - GFG/union-of-two-arrays.cpp
@@ -11,16 +11,16 @@ class Solution{
     public:
     //Function to return the count of number of elements in union of two arrays.
     int doUnion(int a[], int n, int b[], int m)  {
-        //code here
-        set<int> st;
+        //code 
+        unordered_set<int> st;
         for(int i =0;i<n;i++){
             st.insert(a[i]);
         }
-        for(int j =0;j<m;j++){
-            st.insert(b[j]);
+        for(int i =0;i<m;i++){
+            st.insert(b[i]);
         }
-        return st.size();
         
+        return st.size();
     }
 };
 
