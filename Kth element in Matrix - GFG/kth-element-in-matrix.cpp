e9c94b39024249c5ce4271e50611c13dc2c3a644
goelshivam15 <93276658+goelshivam15@users.noreply.github.com>
2023-01-09 23:43:02 +0530

    Added solution - LeetHub

--- Kth element in Matrix - GFG/kth-element-in-matrix.cpp
+++ Kth element in Matrix - GFG/kth-element-in-matrix.cpp
@@ -32,20 +32,20 @@ int main()
 
 int kthSmallest(int mat[MAX][MAX], int n, int k)
 {
-  //Your code here
-  
-  priority_queue<int,vector<int>, greater<int>> pq;
-  for(int i =0;i<n;i++){
-      for(int j =0;j<n;j++){
-          pq.push(mat[i][j]);
-      }
-  }
-  
-  int a = k-1;
-  while(a--){
-      pq.pop();
-  }
-  return pq.top();
-  
-  
+  //Your code 
+    priority_queue<int,vector<int> , greater<int>> pq;
+    for(int i =0;i<n;i++){
+        for(int j =0;j<n;j++){
+            
+            pq.push(mat[i][j]);
+            
+        }
+        
+    }
+    
+    int a = k-1;
+    while(a--){
+        pq.pop();
+    }
+    return pq.top();
 }
