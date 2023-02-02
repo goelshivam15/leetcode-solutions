a1f29b3b81af2aa0a1ebc441ea556b97cd963b8f
goelshivam15 <93276658+goelshivam15@users.noreply.github.com>
2023-02-02 09:12:35 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -10,24 +10,22 @@ class Solution
 {
     public:
     //Function to find the length of longest common subsequence in two strings.
-    int f(int index1, int index2, int x , int y , string s1 , string s2,vector<vector<int>>&dp){
-        if(index1==x || index2==y){
+    int f(int i ,int j ,int x , int y , string s1 , string s2,vector<vector<int>>&dp){
+        if(i==x || j==y){
             return 0;
         }
-        
-        if(dp[index1][index2]!=-1){
-            return dp[index1][index2];
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
         
         int take = INT_MIN;
-        if(s1[index1]==s2[index2]){
-            take = 1+f(index1+1,index2+1,x,y,s1,s2,dp);
+        if(s1[i]==s2[j]){
+            take = 1+f(i+1,j+1,x,y,s1,s2,dp);
         }
         
-        int nottake = max(f(index1+1,index2,x,y,s1,s2,dp),f(index1,index2+1,x,y,s1,s2,dp));
+        int nottake = max(f(i+1,j,x,y,s1,s2,dp),f(i,j+1,x,y,s1,s2,dp));
         
-        
-        return dp[index1][index2]= max(take , nottake);
+        return dp[i][j]= max(take, nottake);
     }
     int lcs(int x, int y, string s1, string s2)
     {
