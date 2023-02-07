bfe8c249f9078a89f4a138491baf3ac567d5b8e8
goelshivam15 <93276658+goelshivam15@users.noreply.github.com>
2023-02-08 00:57:25 +0530

    Added solution - LeetHub

--- Longest subsequence-1 - GFG/longest-subsequence1.cpp
+++ Longest subsequence-1 - GFG/longest-subsequence1.cpp
@@ -10,7 +10,6 @@ using namespace std;
 class Solution{
 public:
     int f(int prev, int index, int n , int a[],vector<vector<int>>&dp){
-        
         if(index==n){
             return 0;
         }
@@ -20,7 +19,7 @@ public:
         
         int take = INT_MIN;
         if(prev==-1 || abs(a[prev]-a[index])==1){
-            take = 1+f(index, index+1,n,a,dp);
+            take = 1+f(index,index+1,n,a,dp);
         }
         
         int nottake = f(prev,index+1,n,a,dp);
@@ -31,7 +30,7 @@ public:
     int longestSubsequence(int n, int a[])
     {
         // code here
-        vector<vector<int>> dp(n,vector<int>(n,-1));
+        vector<vector<int>> dp(n,vector<int> (n,-1));
         return f(-1,0,n,a,dp);
     }
 };
