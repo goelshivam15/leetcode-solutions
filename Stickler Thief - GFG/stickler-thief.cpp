b227467c51d546c3a93393e655e48409ffa19614
goelshivam15 <93276658+goelshivam15@users.noreply.github.com>
2023-02-20 12:02:07 +0530

    Added solution - LeetHub

--- Stickler Thief - GFG/stickler-thief.cpp
+++ Stickler Thief - GFG/stickler-thief.cpp
@@ -8,11 +8,10 @@ class Solution
 {
     public:
     //Function to find the maximum money the thief can get.
-    
-   
     int FindMaxSum(int arr[], int n)
     {
         // Your code here
+        
         vector<int> dp(n,0);
         dp[0]=arr[0];
         for(int i =1;i<n;i++){
@@ -23,8 +22,10 @@ class Solution
             
             int nottake = dp[i-1];
             
+            
             dp[i]= max(take,nottake);
         }
+        
         
         return dp[n-1];
     }
