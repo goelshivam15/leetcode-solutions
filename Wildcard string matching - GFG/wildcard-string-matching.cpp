db89327fdaec617046969058d8282c6dc443ee8f
goelshivam15 <93276658+goelshivam15@users.noreply.github.com>
2023-02-20 14:09:26 +0530

    Added solution - LeetHub

--- Wildcard string matching - GFG/wildcard-string-matching.cpp
+++ Wildcard string matching - GFG/wildcard-string-matching.cpp
@@ -7,7 +7,7 @@ using namespace std;
 class Solution{
     public:
     
-    bool f(int i , int j , string wild , string pattern,vector<vector<int>>&dp){
+    bool f(int i , int j, string wild , string pattern){
         if(i<0 && j<0){
             return true;
         }
@@ -16,37 +16,32 @@ class Solution{
             return false;
         }
         
-        if(j<0 && i>=0){
-            for(int k =0;k<i;i++){
-                if(wild[i] != '*'){
+        if(i>=0 && j<0){
+            for(int k =0;k<i;k++){
+                if(wild[k]!='*'){
                     return false;
                 }
             }
             
             return true;
         }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
+        
         
         if(wild[i]==pattern[j] || wild[i]=='?'){
-            return dp[i][j]= f(i-1,j-1,wild,pattern,dp);
+            return f(i-1,j-1,wild,pattern);
         }
         else if(wild[i]=='*'){
-            return dp[i][j]= f(i-1,j,wild,pattern,dp) or f(i,j-1,wild,pattern,dp);
+            return f(i-1,j,wild,pattern) or f(i,j-1,wild,pattern);
         }
         else{
             return false;
         }
-        
     }
     bool match(string wild, string pattern)
     {
         // code here
-        int n = wild.size();
-        int m = pattern.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return f(wild.size()-1,pattern.size()-1,wild,pattern,dp);
+        
+        return f(wild.size()-1,pattern.size()-1,wild,pattern);
     }
 };
 
