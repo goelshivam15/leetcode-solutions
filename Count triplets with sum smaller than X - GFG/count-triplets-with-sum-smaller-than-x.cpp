cb5a33420bb8301c8d32e84029f0c4a3c8f16d35
goelshivam15 <93276658+goelshivam15@users.noreply.github.com>
2023-03-01 15:56:12 +0530

    Added solution - LeetHub

--- Count triplets with sum smaller than X - GFG/count-triplets-with-sum-smaller-than-x.cpp
+++ Count triplets with sum smaller than X - GFG/count-triplets-with-sum-smaller-than-x.cpp
@@ -10,21 +10,24 @@ class Solution{
 	long long countTriplets(long long arr[], int n, long long sum)
 	{
 	    // Your code goes here
-	    long long count =0;
 	    sort(arr,arr+n);
+	    long long count =0;
 	    for(int i =0;i<n-2;i++){
 	        long long j = i+1;
 	        long long k = n-1;
 	        while(j<k){
-	            if(arr[i]+arr[j]+arr[k]>=sum){
-	                k--;
+	            if(arr[i]+arr[j]+arr[k]<sum){
+	                
+	                count += k-j;
+	                j++;
+	                
 	            }
 	            else{
-	                count+=k-j;
-	                j++;
+	                k--;
 	            }
 	        }
 	    }
+	    
 	    
 	    return count;
 	}
