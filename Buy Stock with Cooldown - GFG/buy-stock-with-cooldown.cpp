6521c3d977e48ac9d6b98f115f1cb6df36d8b0dc
goelshivam15 <93276658+goelshivam15@users.noreply.github.com>
2023-03-08 14:12:11 +0530

    Added solution - LeetHub

--- Buy Stock with Cooldown - GFG/buy-stock-with-cooldown.cpp
+++ Buy Stock with Cooldown - GFG/buy-stock-with-cooldown.cpp
@@ -6,50 +6,35 @@ using namespace std;
 class Solution{
     public:
     
-    long long f(int index,int buy,  vector<long long>& prices, int n,vector<vector<long long>>&dp){
-        if(index>=n){
-            
-            return 0;
-            
-        }
-        
-        if(dp[index][buy]!=-1){
-            return dp[index][buy];
-        }
-        
-        long long  profit =0;
-        if(buy==1){
-            profit = max(-prices[index]+f(index+1,0,prices,n,dp),f(index+1,1,prices,n,dp));
-        }
-        else{
-            profit = max(prices[index]+f(index+2,1,prices,n,dp),f(index+1,0,prices,n,dp));
-        }
-        
-        return dp[index][buy]= profit;
-    }
+   
+    
     long long maximumProfit(vector<long long>&prices, int n) {
         // Code here
         
-        vector<vector<long long>> dp(n+2,vector<long long>(2,0));
-        dp[n][0]=0;
-        dp[n][1]=0;
+        vector<long long> prev2(2,0);
+        vector<long long> prev1(2,0);
+        vector<long long> curr(2,0);
+        prev1[0]=0;
+        prev1[1]=0;
         for(int index = n-1;index>=0;index--){
             for(int buy =0;buy<=1;buy++){
                 
                     long long  profit =0;
                     if(buy==1){
-                        profit = max(-prices[index]+dp[index+1][0],dp[index+1][1]);
+                        profit = max(-prices[index]+prev1[0],prev1[1]);
                     }
                     else{
-                        profit = max(prices[index]+dp[index+2][1],dp[index+1][0]);
+                        profit = max(prices[index]+prev2[1],prev1[0]);
                     }
                 
-                dp[index][buy]= profit;
+                curr[buy]= profit;
                 
             }
+            prev2 = prev1;
+            prev1 = curr;
         }
         
-        return dp[0][1];
+        return prev1[1];
     }
 };
 
