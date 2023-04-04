22472eb46e1d6f3483ff39cb981fb74121dd6301
goelshivam15 <93276658+goelshivam15@users.noreply.github.com>
2023-04-04 10:52:19 +0530

    Added solution - LeetHub

--- Distance of nearest cell having 1 - GFG/distance-of-nearest-cell-having-1.cpp
+++ Distance of nearest cell having 1 - GFG/distance-of-nearest-cell-having-1.cpp
@@ -9,47 +9,48 @@ class Solution
     //Function to find distance of nearest 1 in the grid for each cell.
 	vector<vector<int>>nearest(vector<vector<int>>grid)
 	{
-	    // Code 
-	    
+	    // Code here
 	    int n = grid.size();
 	    int m = grid[0].size();
 	    
-	    
 	    vector<vector<int>> vis(n,vector<int>(m,0));
-	    vector<vector<int>> dis(n,vector<int>(m,0));
-	    
+	    vector<vector<int>> dist(n,vector<int>(m,0));
 	    queue<pair<pair<int,int>,int>> q;
+	    
 	    for(int i =0;i<n;i++){
 	        for(int j =0;j<m;j++){
 	            if(grid[i][j]==1){
-	                vis[i][j]=1;
+	                
+	                dist[i][j]=0;
+	                
 	                q.push({{i,j},0});
 	                
 	            }
 	        }
 	    }
 	    
-	    int delrow[] = {-1,0,+1,0};
-	    int delcol []={0,+1,0,-1};
-	    
+	    int delrow[]={-1,0,+1,0};
+	    int delcol[]={0,+1,0,-1};
 	    while(!q.empty()){
 	        int row = q.front().first.first;
 	        int col = q.front().first.second;
-	        int dist = q.front().second;
+	        int dis = q.front().second;
 	        q.pop();
-	        dis[row][col]=dist;
 	        for(int i =0;i<4;i++){
 	            int nrow = row + delrow[i];
-	            int ncol = col + delcol[i];
-	            if(nrow<n && nrow>=0 && ncol<m && ncol>=0 && !vis[nrow][ncol] && grid[nrow][ncol]==0){
-	                q.push({{nrow,ncol},dist+1});
+	            int ncol = col+ delcol[i];
+	            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==0 && !vis[nrow][ncol]){
 	                vis[nrow][ncol]=1;
+	                dist[nrow][ncol]= dis+1;
+	                q.push({{nrow,ncol},dis+1});
 	            }
 	        }
 	        
+	        
+	        
 	    }
 	    
-	    return dis;
+	    return dist;
 	}
 };
 
