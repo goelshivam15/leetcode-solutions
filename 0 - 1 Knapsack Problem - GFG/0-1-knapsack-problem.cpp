9b5c44d876050e081ee05e86431c4e8604c74aef
goelshivam15 <93276658+goelshivam15@users.noreply.github.com>
2023-06-01 15:09:30 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -8,27 +8,54 @@ class Solution
 {
     public:
     //Function to return max value that can be put in knapsack of capacity W.
-    int f(int index , int w , int wt[] , int val[] ,int n,vector<vector<int>>&dp){
-        if(index==n){
-            return 0;
-        }
-        if(dp[index][w]!=-1){
-            return dp[index][w];
+    
+    
+    int f(int index , int w , int wt[] , int val[] , int n){
+        if(index==0){
+            if(wt[0]<=w){
+                return val[0];
+            }
+            else{
+                return 0;
+            }
         }
         
+        int nottake = f(index-1,w,wt,val,n);
         int take = INT_MIN;
         if(wt[index]<=w){
-            take = val[index]+f(index+1,w-wt[index],wt,val,n,dp);
+            take = val[index]+ f(index-1,w-wt[index],wt,val,n);
         }
         
-        int nottake = f(index+1,w,wt,val,n,dp);
-        return dp[index][w]= max(take , nottake);
+        
+        return max(take,nottake);
     }
     int knapSack(int w, int wt[], int val[], int n) 
     { 
        // Your code here
-       vector<vector<int>> dp(n,vector<int>(w+1,-1));
-       return f(0,w,wt,val,n,dp);
+       
+      vwe
+       
+       for(int i = wt[0];i<=w;i++){
+           dp[0][i]= val[0];
+       }
+       
+       
+       for(int index = 1;index<n;index++){
+           for(int tar = 0;tar<=w;tar++){
+               int nottake = dp[index-1][tar];
+               
+               int take = INT_MIN;
+               if(wt[index]<=tar){
+                   take = val[index]+dp[index-1][tar-wt[index]];
+               }
+               
+               
+               dp[index][tar]= max(take,nottake);
+           }
+       }
+       
+       
+       return dp[n-1][w];
     }
 };
 
