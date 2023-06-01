1613f0d58f1b8827d5fd26355008dd4513898442
goelshivam15 <93276658+goelshivam15@users.noreply.github.com>
2023-06-01 22:51:35 +0530

    Added solution - LeetHub

--- Max Sum without Adjacents 2 - GFG/max-sum-without-adjacents-2.cpp
+++ Max Sum without Adjacents 2 - GFG/max-sum-without-adjacents-2.cpp
@@ -11,30 +11,44 @@ using namespace std;
 
 class Solution{
   public:
-  int f(int index, vector<int>&a,vector<int>&dp){
-      if(index>=a.size()){
+  
+  int f(int index , vector<int>&a){
+      int n = a.size();
+      if(index>=n){
           return 0;
       }
       
-      if(index==a.size()-1){
-          return a[index];
-      }
-      
-      if(dp[index]!=-1){
-          return dp[index];
-      }
-      
-      
-      int op1 = a[index]+a[index+1]+f(index+3,a,dp);
-      int op2 = a[index]+f(index+2,a,dp);
-      int op3 = f(index+1,a,dp);
-      
-      return dp[index]= max(op1,max(op2,op3));
+     int op1 = a[index]+a[index+1]+f(index+3,a);
+     int op2 = a[index]+f(index+2,a);
+     int op3 = f(index+1,a);
+     
+     return max(op1,max(op2,op3));
   }
   int findMaxSum(vector<int>& a){
       //Code Here
-      vector<int> dp(a.size(),-1);
-      return f(0,a,dp);
+      int n = a.size();
+      vector<int> dp(n,0);
+      
+      dp[n-1]=a[n-1];
+      
+      for(int i = n-2;i>=0;i--){
+          int op1 = a[i]+a[i+1];
+          if(i+3<n){
+              op1 += dp[i+3];
+          }
+          
+          int op2 = a[i];
+          if(i+2<n){
+              op2 += dp[i+2];
+          }
+          
+         int op3 = dp[i+1];
+          
+          dp[i]= max(op1,max(op2,op3));
+          
+      }
+      
+      return dp[0];
   }
 };
 
