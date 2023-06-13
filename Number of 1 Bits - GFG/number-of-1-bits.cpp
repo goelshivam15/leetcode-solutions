d7f3ee23de62aa07b8d21d02ca26eeec9f5d1d83
goelshivam15 <93276658+goelshivam15@users.noreply.github.com>
2023-06-14 04:34:48 +0530

    Added solution - LeetHub

--- Number of 1 Bits - GFG/number-of-1-bits.cpp
+++ Number of 1 Bits - GFG/number-of-1-bits.cpp
@@ -7,13 +7,17 @@ class Solution {
   public:
     int setBits(int n) {
         // Write Your Code here
-        int count =0;
-        while(n){
-            n = (n)&(n-1);
-            count++;
+        int cnt =0;
+        while(n>0){
+           if(n&1){
+               cnt++;
+           }
+           
+           n = n>>1;
+            
         }
         
-        return count;
+        return cnt;
     }
 };
 
