951b5beae0ac197ba36dff946bee90e8e0639adb
goelshivam15 <93276658+goelshivam15@users.noreply.github.com>
2023-06-29 23:50:11 +0530

    Added solution - LeetHub

--- Maximum Rectangular Area in a Histogram - GFG/maximum-rectangular-area-in-a-histogram.cpp
+++ Maximum Rectangular Area in a Histogram - GFG/maximum-rectangular-area-in-a-histogram.cpp
@@ -7,68 +7,64 @@ using namespace std;
 class Solution
 {
     public:
+     vector<long long > nextSmallerElement(long long arr[], int n) {
+        stack<int> s;
+        s.push(-1);
+        vector<long long> ans(n);
+
+        for(int i=n-1; i>=0 ; i--) {
+            int curr = arr[i];
+            while(s.top() != -1 && arr[s.top()] >= curr)
+            {
+                s.pop();
+            }
+            //ans is stack ka top
+            ans[i] = s.top();
+            s.push(i);
+        }
+        return ans;
+    }
+    
+      vector<long long> prevSmallerElement(long long  arr[], int n) {
+        stack<int> s;
+        s.push(-1);
+        vector<long long > ans(n);
+
+        for(int i=0; i<n; i++) {
+            int curr = arr[i];
+            while(s.top() != -1 && arr[s.top()] >= curr)
+            {
+                s.pop();
+            }
+            //ans is stack ka top
+            ans[i] = s.top();
+            s.push(i);
+        }
+        return ans; 
+    }
     //Function to find largest rectangular area possible in a given histogram.
-    vector<long long> prevsmallerelement(long long arr[],int n){
-        
-        vector<long long> ans(n);
-        stack<long long> s;
-
-        s.push(-1);
-        for(long long i =0;i<n;i++){
-            long long  curr = arr[i];
-            while(s.top()!=-1 && arr[s.top()]>=curr){
-                s.pop();
-
-            }
-            ans[i]=s.top();
-            s.push(i);
-        }
-
-        return ans;
-    }
-    vector<long long> nextsmallerelement(long long arr[],int n){
-        
-        vector<long long> ans(n);
-        stack<long long> s;
-
-        s.push(-1);
-        for(long long i =n-1;i>=0;i--){
-            long long curr = arr[i];
-            while(s.top()!=-1 && arr[s.top()]>=curr){
-                s.pop();
-
-            }
-            ans[i]=s.top();
-            s.push(i);
-        }
-
-        return ans;
-    }
     long long getMaxArea(long long arr[], int n)
     {
         // Your code here
+        vector<long long > next(n);
+        next = nextSmallerElement(arr, n);
+            
+        vector<long long > prev(n);
+        prev = prevSmallerElement(arr, n);
         
-
-        vector<long long> prev(n);
-        vector<long long> next(n);
-        prev = prevsmallerelement(arr,n);
-        next =  nextsmallerelement(arr,n);
-        long long area = INT_MIN;
-
-
-        for(long long i =0;i<n;i++){
-
-            long long length = arr[i];
-            if(next[i]==-1){
-                next[i]=n;
+        long long  area = INT_MIN;
+        for(int i=0; i<n; i++) {
+            long long  l = arr[i];
+            
+            if(next[i] == -1) {
+                next[i] = n;
             }
-
-            long long width = next[i]-prev[i]-1;
-            long long newarea = length*width;
-            area = max(area,newarea);
+             long long b = next[i] - prev[i] - 1;
+            long long  newArea = l*b;
+            area = max(area, newArea);
         }
-
         return area;
+        
     }
 };
 
