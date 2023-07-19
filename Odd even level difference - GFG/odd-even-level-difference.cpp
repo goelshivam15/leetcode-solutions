266aeb5cdcf6f820133fc33d993e7163d36947d5
goelshivam15 <93276658+goelshivam15@users.noreply.github.com>
2023-07-19 15:09:23 +0530

    Added solution - LeetHub

---
+++ Odd even level difference - GFG/odd-even-level-difference.cpp
@@ -0,0 +1,171 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+
+// } Driver Code Ends
+/* The Node structure is
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+}; */
+
+class Solution{
+  public:
+    /*You are required to complete this function*/
+    int getLevelDiff(Node *root)
+    {
+       //Your code here
+       int even = 0;
+       int odd = 0;
+       if(root==NULL){
+           return 0;
+       }
+       
+       queue<Node*> q;
+       q.push(root);
+       int lvl = 0;
+       while(!q.empty()){
+           lvl +=1;
+           
+           
+           int size = q.size();
+           for(int i =0;i<size;i++){
+               Node* curr = q.front();
+               q.pop();
+              if(lvl%2==0){
+                  even += curr->data;
+              }
+              else{
+                  odd += curr->data;
+              }
+              
+              if(curr->left){
+                  q.push(curr->left);
+              }
+              if(curr->right){
+                  q.push(curr->right);
+              }
+           }
+           
+          
+           
+       }
+       
+       return odd-even;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s ,ch;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        Solution obj;
+        cout<<obj.getLevelDiff(root);
+        cout << endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
